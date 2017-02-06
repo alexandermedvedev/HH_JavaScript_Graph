d4a35cb859192af35e6b929f32543ef85cb33f58
alexandermedvedev <alex.medvedev@hotmail.com>
2017-02-06 13:21:50 +0300

    Lazy calculation refactoring

--- graph.js
+++ graph.js
@@ -1,43 +1,42 @@
 function Vertex(dependencies, calculatedFunction){
 	this.dependencies = dependencies;
 	this.calculatedFunction = calculatedFunction;
-	this.result;
-	this.isCheckingForLoop = false;
+	this.wasChecked = false;
 }
 
-var example = {};
-example.xs = new Vertex([], ([]) => [1,2,3,6]);
-example.n = new Vertex(['xs'], function([xs]) {return xs.length});
-example.m = new Vertex(['xs', 'n'], function([xs, n]) {return xs.reduce((sum, cur) => sum + cur) / n});
-example.m2 = new Vertex(['xs', 'n'], function([xs, n]) {return xs.reduce((sum, cur) => sum + cur * cur) / n});
-example.v = new Vertex(['m', 'm2'], function([m, m2]) {return m2 - m * m});
+var example = {
+xs: new Vertex([], ([]) => [1,2,3,6]),
+n: new Vertex(['xs'], ([xs]) => xs.length),
+m: new Vertex(['xs', 'n'], ([xs, n]) => xs.reduce((sum, cur) => sum + cur) / n),
+m2: new Vertex(['xs', 'n'], ([xs, n]) => xs.reduce((sum, cur) => sum + cur * cur) / n),
+v: new Vertex(['m', 'm2'], ([m, m2]) => m2 - m * m)
+};
 
+var loopExample = {
+a: new Vertex(['b'], ([b]) => b++),
+b: new Vertex(['a'], ([a]) => a++)
+};
 
-var loopExample = {};
-loopExample.a = new Vertex(['b'], function([b]) {return b++});
-loopExample.b = new Vertex(['a'], function([a]) {return a++});
-
-function lazyCalculation(vertexName, graph){
+function calculateLazy(vertexName, graph){
 	var v = graph[vertexName];
 	
 	if (v === undefined){
 		throw new Error("Calculation failed: Vertex \"" + vertexName + "\" is not presented in the graph");
 	}
 	
-	if (v.isCheckingForLoop === true){
-		throw new Error("Calculation failed: Loop with vertex \"" + vertexName + "\" was found during the calculation");
-	}
-	else{
-		v.isCheckingForLoop = true;
-	}
-	
 	if (v.result === undefined){
-	
-		var dependenciesResults = new Array();
-	
+		
+		if (v.wasChecked === true){
+			throw new Error("Calculation failed: Loop with vertex \"" + vertexName + "\" was found during the calculation");
+		}
+		
+		v.wasChecked = true;
+		
+		var dependenciesResults = [];
+				
 		for (var i = 0; i < v.dependencies.length; i++){
 			if (graph[v.dependencies[i]].result === undefined){
-				dependenciesResults.push(lazyCalculation(v.dependencies[i], graph));
+				dependenciesResults.push(calculateLazy(v.dependencies[i], graph));
 			} 
 			else{
 				dependenciesResults.push(graph[v.dependencies[i]].result);
@@ -47,28 +46,26 @@ function lazyCalculation(vertexName, graph){
 		v.result = v.calculatedFunction(dependenciesResults);
 	}
 	
-	v.isCheckingForLoop = false;
 	return v.result;
 }
 
 function calculateAll(graph){
 	for (var v in graph){
-		var result = lazyCalculation(v, graph);
+		var result = calculateLazy(v, graph);
 		console.log(v + ": "  + result);
 	}
 }
+//console.log("a: ", calculateLazy('a', loopExample));
+//console.log("a: ", calculateLazy('a', example));
 
-//console.log("a: ", lazyCalculation('a', loopExample));
-//console.log("a: ", lazyCalculation('a', example));
+console.log("xs: ", calculateLazy('xs', example));
+console.log("xs: ", calculateLazy('xs', example));
+console.log("n: ", calculateLazy('n', example));
+console.log("m: ", calculateLazy('m', example));
+console.log("m2: ", calculateLazy('m2', example));
+console.log("v: ", calculateLazy('v', example));
 
-//console.log("xs: ", lazyCalculation('xs', example));
-//console.log("n: ", lazyCalculation('n', example));
-//console.log("m: ", lazyCalculation('m', example));
-//console.log("m2: ", lazyCalculation('m2', example));
-//console.log("v: ", lazyCalculation('v', example));
 
-/*
 console.log("Calculate all started");
 calculateAll(example);
 console.log("Calculate all finished");
-*/
